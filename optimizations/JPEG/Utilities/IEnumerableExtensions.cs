cd098edc9ee3b0596d4d39a6f1d2320a6c373da2
Ilya_Shumilin <i.sh-vk@yandex.ru>
2021-04-05 20:50:19 +0500

    small fix

--- optimizations/JPEG/Utilities/IEnumerableExtensions.cs
+++ optimizations/JPEG/Utilities/IEnumerableExtensions.cs
@@ -4,21 +4,28 @@ using System.Linq;
 
 namespace JPEG.Utilities
 {
-	static class IEnumerableExtensions
-	{
-		public static T MinOrDefault<T>(this IEnumerable<T> enumerable, Func<T, int> selector)
-		{
-			return enumerable.OrderBy(selector).FirstOrDefault();
-		}
+    static class IEnumerableExtensions
+    {
+        public static T MinOrDefault<T>(this IEnumerable<T> enumerable, Func<T, int> selector)
+        {
+            var valueKey = (Value: default(T), Key: int.MaxValue);
+            foreach (var e in enumerable)
+            {
+                var key = selector(e);
+                if (key < valueKey.Key)
+                    valueKey = (e, key);
+            }
+            return valueKey.Value;
+        }
 
-		public static IEnumerable<T> Without<T>(this IEnumerable<T> enumerable, params T[] elements)
-		{
-			return enumerable.Where(x => !elements.Contains(x));
-		}
-		
-		public static IEnumerable<T> ToEnumerable<T>(this T element)
-		{
-			yield return element;
-		}
-	}
+        public static IEnumerable<T> Without<T>(this IEnumerable<T> enumerable, params T[] elements)
+        {
+            return enumerable.Where(x => !elements.Contains(x));
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this T element)
+        {
+            yield return element;
+        }
+    }
 }
