183d8b4d39a7d67b45ecbc9afe6d57ac162e4f6f
Ilya_Shumilin <i.sh-vk@yandex.ru>
2021-04-05 20:45:51 +0500

    refactor:     - unsafe matrix indexer     - enum PixelFormat     - pretty Pixel ctor

--- optimizations/JPEG/Images/Matrix.cs
+++ optimizations/JPEG/Images/Matrix.cs
@@ -1,66 +1,88 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
 
 namespace JPEG.Images
 {
-    class Matrix
+    unsafe class Matrix : IDisposable
     {
-        public readonly Pixel[,] Pixels;
         public readonly int Height;
         public readonly int Width;
-				
-        public Matrix(int height, int width)
+        private readonly Bitmap bitmap;
+        private readonly BitmapData bitmapData;
+        private readonly int bytesPerPixel;
+        private readonly int widthInBytes;
+        private readonly byte* ptrFirstPixel;
+        private readonly int stride;
+
+        public Matrix(int height, int width, Bitmap bitmap)
         {
             Height = height;
             Width = width;
-			
-            Pixels = new Pixel[height,width];
-            for(var i = 0; i< height; ++i)
-            for(var j = 0; j< width; ++j)
-                Pixels[i, j] = new Pixel(0, 0, 0, PixelFormat.RGB);
+            this.bitmap = bitmap;
+            bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                        ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            bytesPerPixel = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            widthInBytes = Width * bytesPerPixel;
+            ptrFirstPixel = (byte*)bitmapData.Scan0;
+            stride = bitmapData.Stride;
         }
+
+        public Pixel this[int y, int x]
+        {
+            get
+            {
+                Pixel pixel;
+                byte* currentLine = ptrFirstPixel + (y * stride);
+                var byteOffset = x * bytesPerPixel;
+                pixel = new Pixel(
+                        currentLine[byteOffset + 2],
+                        currentLine[byteOffset + 1],
+                        currentLine[byteOffset], PixelFormat.RGB);
+                return pixel;
+            }
+
+            set
+            {
+                byte* currentLine = ptrFirstPixel + (y * stride);
+                var byteOffset = x * bytesPerPixel;
+                var pixel = value;
+                currentLine[byteOffset + 2] = (byte)ToByte(pixel.R);
+                currentLine[byteOffset + 1] = (byte)ToByte(pixel.G);
+                currentLine[byteOffset] = (byte)ToByte(pixel.B);
+            }
+        }
+
 
         public static explicit operator Matrix(Bitmap bmp)
         {
             var height = bmp.Height - bmp.Height % 8;
             var width = bmp.Width - bmp.Width % 8;
-            var matrix = new Matrix(height, width);
-
-            for(var j = 0; j < height; j++)
-            {
-                for(var i = 0; i < width; i++)
-                {
-                    var pixel = bmp.GetPixel(i, j);
-                    matrix.Pixels[j, i] = new Pixel(pixel.R, pixel.G, pixel.B, PixelFormat.RGB);
-                }
-            }
-
+            var matrix = new Matrix(height, width, bmp);
             return matrix;
         }
 
         public static explicit operator Bitmap(Matrix matrix)
         {
-            var bmp = new Bitmap(matrix.Width, matrix.Height);
-
-            for(var j = 0; j < bmp.Height; j++)
-            {
-                for(var i = 0; i < bmp.Width; i++)
-                {
-                    var pixel = matrix.Pixels[j, i];
-                    bmp.SetPixel(i, j, Color.FromArgb(ToByte(pixel.R), ToByte(pixel.G), ToByte(pixel.B)));
-                }
-            }
-
-            return bmp;
+            return matrix.bitmap;
         }
 
         public static int ToByte(double d)
         {
-            var val = (int) d;
+            var val = (int)d;
             if (val > byte.MaxValue)
                 return byte.MaxValue;
             if (val < byte.MinValue)
                 return byte.MinValue;
             return val;
+        }
+
+        public void Dispose()
+        {
+            bitmap.UnlockBits(bitmapData);
+            bitmap.Dispose();
         }
     }
 }
--- optimizations/JPEG/Images/Pixel.cs
+++ optimizations/JPEG/Images/Pixel.cs
@@ -3,26 +3,30 @@ using System.Linq;
 
 namespace JPEG.Images
 {
-    public class Pixel
+    public struct Pixel
     {
-        private readonly PixelFormat format;
-
-        public Pixel(double firstComponent, double secondComponent, double thirdComponent, PixelFormat pixelFormat)
+        public Pixel(double firstComponent, double secondComponent,
+            double thirdComponent, PixelFormat pixelFormat) : this()
         {
-            if (!new[]{PixelFormat.RGB, PixelFormat.YCbCr}.Contains(pixelFormat))
+            if (PixelFormat.RGB != pixelFormat && PixelFormat.YCbCr != pixelFormat)
                 throw new FormatException("Unknown pixel format: " + pixelFormat);
-            format = pixelFormat;
             if (pixelFormat == PixelFormat.RGB)
             {
                 r = firstComponent;
                 g = secondComponent;
                 b = thirdComponent;
+                y = 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
+                cb = 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
+                cr = 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
             }
             if (pixelFormat == PixelFormat.YCbCr)
             {
                 y = firstComponent;
                 cb = secondComponent;
                 cr = thirdComponent;
+                r = (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
+                g = (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
+                b = (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
             }
         }
 
@@ -34,12 +38,12 @@ namespace JPEG.Images
         private readonly double cb;
         private readonly double cr;
 
-        public double R => format == PixelFormat.RGB ? r : (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
-        public double G => format == PixelFormat.RGB ? g : (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
-        public double B => format == PixelFormat.RGB ? b : (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
+        public double R => r;
+        public double G => g;
+        public double B => b;
 
-        public double Y => format == PixelFormat.YCbCr ? y : 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
-        public double Cb => format == PixelFormat.YCbCr ? cb : 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
-        public double Cr => format == PixelFormat.YCbCr ? cr : 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+        public double Y => y;
+        public double Cb => cb;
+        public double Cr => cr;
     }
 }
